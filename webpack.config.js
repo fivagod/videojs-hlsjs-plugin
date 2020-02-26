615f57274b8cd0efb1f344dcd299cb82bcbc7482
Ivan Sologub <ivan.sologub@gmail.com>
2020-02-26 22:28:58 +0200

    stop loading chunks in live

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
-
-const { version } = require('./package.json');
+const pkgJson = require('./package.json');
 
 const sourcePath = path.resolve(__dirname, 'lib/');
 const distPath = path.resolve(__dirname, 'dist/');
+const importHelper = require('@babel/helper-module-imports');
 
-module.exports = (env) => {
+/* Allow to customise builds through env-vars */
+const env = process.env;
+const addSubtitleSupport = !!env.SUBTITLE || !!env.USE_SUBTITLES;
+const addAltAudioSupport = !!env.ALT_AUDIO || !!env.USE_ALT_AUDIO;
+const addEMESupport = !!env.EME_DRM || !!env.USE_EME_DRM;
+
+const createDefinePlugin = (type) => {
+  const buildConstants = {
+    __VERSION__: JSON.stringify(pkgJson.version),
+    __USE_SUBTITLES__: JSON.stringify(type === 'main' || addSubtitleSupport),
+    __USE_ALT_AUDIO__: JSON.stringify(type === 'main' || addAltAudioSupport),
+    __USE_EME_DRM__: JSON.stringify(type === 'main' || addEMESupport)
+  };
+  return new webpack.DefinePlugin(buildConstants);
+};
+module.exports = () => {
     let nodeEnv = 'production';
-
-    if (env && env.development) {
+    if (env && env.NODE_ENV === 'development') {
         nodeEnv = 'development';
-    } else if (env && env.staging) {
-        nodeEnv = 'staging';
-    } else if (env && env.features) {
-        nodeEnv = 'features';
     }
 
     const isDev = nodeEnv === 'development';
 
     const plugins = [
-        new webpack.DefinePlugin({
-            _VERSION_: JSON.stringify(version),
-        }),
+        createDefinePlugin('main')
     ];
 
     if (!isDev) {
@@ -59,19 +67,55 @@ module.exports = (env) => {
         target: 'web',
         output: {
             path: distPath,
-            filename: 'videojs-hlsjs-plugin.js',
+            filename: isDev ? 'videojs-hlsjs-plugin-full.js' : 'videojs-hlsjs-plugin.js',
             publicPath: '/dist',
             library: 'videojs-hlsjs-plugin',
             libraryTarget: 'umd',
         },
+        resolve: {
+            // Add `.ts` as a resolvable extension.
+            extensions: ['.ts', '.js']
+        },        
         module: {
             rules: [{
-                test: /\.js$/,
-                exclude: [
-                    /node_modules/,
-                ],
-                use: ['babel-loader'],
-            }],
+                test: /\.(ts|js)$/,
+                loader: 'babel-loader',
+                options: {
+                  babelrc: false,
+                  presets: [
+                    '@babel/preset-typescript',
+                    ['@babel/preset-env', {
+                      loose: true,
+                      modules: false,
+                      targets: {
+                        browsers: [
+                          'chrome >= 47',
+                          'firefox >= 51',
+                          'ie >= 11',
+                          'safari >= 8',
+                          'ios >= 8',
+                          'android >= 4'
+                        ]
+                      }
+                    }]
+                  ],
+                  plugins: [
+                    ['@babel/plugin-proposal-class-properties', {
+                      loose: true
+                    }],
+                    '@babel/plugin-proposal-object-rest-spread',
+                    {
+                      visitor: {
+                        CallExpression: function (espath) {
+                          if (espath.get('callee').matchesPattern('Number.isFinite')) {
+                            espath.node.callee = importHelper.addNamed(espath, 'isFiniteNumber', path.resolve('src/polyfills/number-isFinite'));
+                          }
+                        }
+                      }
+                    }
+                  ]
+                }
+              }],
         },
 
         plugins,
